5873738da3499af959314163116d7a47d3e05a7f
Abhijit Kayal <129577364+abhijitkayal10@users.noreply.github.com>
2025-07-04 15:59:41 +0530

    Add files via upload

---
+++ Longest Common Prefix.js
@@ -0,0 +1,15 @@
+
+var longestCommonPrefix = function(strs) {
+    strs.sort();
+let first = strs[0];
+let last = strs[strs.length-1];
+let minLength = Math.min(first.length, last.length);
+
+let i = 0;
+while(i<minLength && first[i]==last[i]){
+    i++;
+}
+
+
+return first.substring(0,i);
+};
